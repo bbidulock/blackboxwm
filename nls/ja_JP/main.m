f32a199d98172502390f9706efb2f6c3e8aee639
shaleh <shaleh>
2002-01-25 09:45:28 +0000

    Initial revision

---
+++ nls/ja_JP/main.m
@@ -0,0 +1,21 @@
+$set 14 #main
+
+$ #RCRequiresArg
+# エラー: '-rc' オプションは引数を必要とします\n
+$ #DISPLAYRequiresArg
+# エラー: '-display' オプションは引数を必要とします\n
+$ #WarnDisplaySet
+# 警告: 環境変数 'DISPLAY' を設定できませんでした\n
+$ #Usage
+# Blackbox %s: (c) 1997 - 2000 Brad Hughes\n\n\
+  -display <string>\t\t指定ディスプレイに接続\n\
+  -rc <string>\t\t\t代りのリソースファイルを使用\n\
+  -version\t\t\tバージョン情報を表示し、終了\n\
+  -help\t\t\t\tこのヘルプを表示し、終了\n\n
+$ #CompileOptions
+# コンパイル時オプション:\n\
+  Debugging\t\t\t%s\n\
+  Interlacing:\t\t\t%s\n\
+  Shape:\t\t\t%s\n\
+  Slit:\t\t\t\t%s\n\
+  8bpp Ordered Dithering:\t%s\n\n
