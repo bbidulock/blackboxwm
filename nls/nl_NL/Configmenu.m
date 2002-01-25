0d488fd6f0976c2e61d49dac6e70e2f67b23a041
shaleh <shaleh>
2002-01-25 10:14:26 +0000

    Initial revision

---
+++ nls/nl_NL/Configmenu.m
@@ -0,0 +1,38 @@
+$set 3 #Configmenu
+
+$ #ConfigOptions
+# Instellingen
+$ #FocusModel
+# Toetsenbord focusmodel
+$ #WindowPlacement
+# Plaatsing van vensters
+$ #ImageDithering
+# Kleurbenadering
+$ #OpaqueMove
+# Inhoud vensters zichtbaar tijdens slepen
+$ #FullMax
+# Volledig maximaliseren
+$ #FocusNew
+# Geef nieuwe vensters direct toetsenbordfocus
+$ #FocusLast
+# Geef laatste venster toetsenbordfocus bij werkblad wisseling
+$ #ClickToFocus
+# Geef focus na muisklik in venster
+$ #SloppyFocus
+# Geef focus zodra muis in venster komt
+$ #AutoRaise
+# Vensters automatisch naar voorgrond
+$ #SmartRows
+# Optimale plaatsing (rijen)
+$ #SmartCols
+# Optimale plaatsing (kolommen)
+$ #Cascade
+# Trapsgewijs
+$ #LeftRight
+# Van links naar rechts
+$ #RightLeft
+# Van rechts naar links
+$ #TopBottom
+# Van boven naar beneden
+$ #BottomTop
+# Van beneden naar boven
