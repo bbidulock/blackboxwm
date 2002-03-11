3290f52c0ee6991f082860a3820bcbc8d41032fd
bradleyhughes <bradleyhughes>
2002-03-11 06:26:26 +0000

    confirm to the blackbox code style

--- src/Widget.cc
+++ src/Widget.cc
@@ -34,47 +34,47 @@
 Mapper Widget::mapper;
 
 
-Widget::Widget( int s, Type t )
-  : _parent( 0 ), _type( t ),
-    visible( false ), focused( false ),
-    grabbedMouse( false ) , grabbedKeyboard( false ),
-    _screen( s )
+Widget::Widget(int s, Type t)
+  : _parent(0), _type(t),
+    visible(false), focused(false),
+    grabbedMouse(false) , grabbedKeyboard(false),
+    _screen(s)
 {
   create();
 }
 
-Widget::Widget( Widget *p )
-  : _parent( p ), _type( Normal ),
-    visible( false ), focused( false ),
-    grabbedMouse( false ) , grabbedKeyboard( false ),
-    _screen( p->screenNumber() )
+Widget::Widget(Widget *p)
+  : _parent(p), _type(Normal),
+    visible(false), focused(false),
+    grabbedMouse(false) , grabbedKeyboard(false),
+    _screen(p->screenNumber())
 {
+  parent()->insertChild(this);
   create();
-  parent()->insertChild( this );
 }
 
 Widget::~Widget()
 {
-  if ( isVisible() )
+  if (isVisible())
     hide();
   // delete children
-  while ( _children.first() )
+  while (_children.first())
     delete _children.first();
-  if ( parent() )
-    parent()->removeChild( this );
-  mapper.erase( win );
-  XDestroyWindow( *BaseDisplay::instance(), win );
+  if (parent())
+    parent()->removeChild(this);
+  mapper.erase(win);
+  XDestroyWindow(*BaseDisplay::instance(), win);
 }
 
 void Widget::create()
 {
     BaseDisplay *display = BaseDisplay::instance();
-    ScreenInfo *screeninfo = display->screenInfo( screenNumber() );
+    ScreenInfo *screeninfo = display->screenInfo(screenNumber());
     Window p = parent() ? parent()->win : screeninfo->rootWindow();
 
     // set the initial geometry
-    _rect.setRect( screeninfo->width() / 4, screeninfo->height() / 4,
-		   screeninfo->width() / 2, screeninfo->height() / 2 );
+    _rect.setRect(screeninfo->width() / 4, screeninfo->height() / 4,
+		   screeninfo->width() / 2, screeninfo->height() / 2);
 
     // create the window
     XSetWindowAttributes attrib;
@@ -92,7 +92,7 @@ void Widget::create()
 			StructureNotifyMask;
 
     // handle the different window type parameters
-    switch( type() ) {
+    switch(type()) {
     case Normal:
 	attrib.override_redirect = False;
 	break;
@@ -107,202 +107,202 @@ void Widget::create()
 	break;
     }
 
-    win = XCreateWindow( *display, p, x(), y(), width(), height(), 0,
+    win = XCreateWindow(*display, p, x(), y(), width(), height(), 0,
 			 screeninfo->depth(), InputOutput,
 			 screeninfo->visual(), mask, &attrib);
 
-    setTitle( "Untitled" );
+    setTitle("Untitled");
 
-    mapper.insert( std::pair<Window,Widget*>( win, this ) );
+    mapper.insert(std::pair<Window,Widget*>(win, this));
 }
 
-void Widget::insertChild( Widget *child )
+void Widget::insertChild(Widget *child)
 {
-    _children.insert( child );
+    _children.insert(child);
 }
 
-void Widget::removeChild( Widget *child )
+void Widget::removeChild(Widget *child)
 {
-    _children.remove( child );
+    _children.remove(child);
 }
 
-void Widget::move( int x, int y )
+void Widget::move(int x, int y)
 {
-    _rect.setPos( x, y );
-    XMoveWindow( *BaseDisplay::instance(), win, x, y );
+    _rect.setPos(x, y);
+    XMoveWindow(*BaseDisplay::instance(), win, x, y);
 }
 
-void Widget::move( const Point &p )
+void Widget::move(const Point &p)
 {
-    move( p.x(), p.y() );
+    move(p.x(), p.y());
 }
 
-void Widget::resize( int w, int h )
+void Widget::resize(int w, int h)
 {
-    _rect.setSize( w, h );
-    XResizeWindow( *BaseDisplay::instance(), win, w, h );
+    _rect.setSize(w, h);
+    XResizeWindow(*BaseDisplay::instance(), win, w, h);
 }
 
-void Widget::resize( const Size &s )
+void Widget::resize(const Size &s)
 {
-    resize( s.width(), s.height() );
+    resize(s.width(), s.height());
 }
 
-void Widget::setGeometry( int x, int y, int w, int h )
+void Widget::setGeometry(int x, int y, int w, int h)
 {
-    _rect = Rect( x, y, w, h );
-    XMoveResizeWindow( *BaseDisplay::instance(), win, x, y, w, h );
+    _rect = Rect(x, y, w, h);
+    XMoveResizeWindow(*BaseDisplay::instance(), win, x, y, w, h);
 }
 
-void Widget::setGeometry( const Point &p, const Size &s )
+void Widget::setGeometry(const Point &p, const Size &s)
 {
-    setGeometry( p.x(), p.y(), s.width(), s.height() );
+    setGeometry(p.x(), p.y(), s.width(), s.height());
 }
 
-void Widget::setGeometry( const Rect &r )
+void Widget::setGeometry(const Rect &r)
 {
-    setGeometry( r.x(), r.y(), r.width(), r.height() );
+    setGeometry(r.x(), r.y(), r.width(), r.height());
 }
 
 void Widget::show()
 {
-    if ( isVisible() )
-	return;
-    LinkedListIterator<Widget> it( &_children );
-    while ( it.current() ) {
-	it.current()->show();
-	it++;
-    }
-    if ( type() == Popup ) {
-	XMapRaised( *BaseDisplay::instance(), win );
-	BaseDisplay::instance()->popup( this );
-    } else
-	XMapWindow( *BaseDisplay::instance(), win );
-    visible = true;
+  if (isVisible())
+    return;
+  LinkedListIterator<Widget> it(&_children);
+  while (it.current()) {
+    it.current()->show();
+    it++;
+  }
+  if (type() == Popup) {
+    XMapRaised(*BaseDisplay::instance(), win);
+    BaseDisplay::instance()->popup(this);
+  } else
+    XMapWindow(*BaseDisplay::instance(), win);
+  visible = true;
 }
 
 void Widget::hide()
 {
-    if ( ! isVisible() )
+    if (! isVisible())
 	return;
-    if ( type() == Popup )
-	BaseDisplay::instance()->popdown( this );
-    XUnmapWindow( *BaseDisplay::instance(), win );
+    if (type() == Popup)
+	BaseDisplay::instance()->popdown(this);
+    XUnmapWindow(*BaseDisplay::instance(), win);
     visible = false;
 }
 
 void Widget::setFocus()
 {
-    if ( ! isVisible() )
+    if (! isVisible())
 	return;
-    XSetInputFocus( *BaseDisplay::instance(), win,
-		    RevertToParent, CurrentTime );
+    XSetInputFocus(*BaseDisplay::instance(), win,
+		    RevertToParent, CurrentTime);
 }
 
-void Widget::setTitle( const string &t )
+void Widget::setTitle(const string &t)
 {
     _title = t;
 }
 
 bool Widget::grabMouse()
 {
-    int ret = XGrabPointer( *BaseDisplay::instance(), win, True,
-			    ( ButtonPressMask | ButtonReleaseMask |
+    int ret = XGrabPointer(*BaseDisplay::instance(), win, True,
+			    (ButtonPressMask | ButtonReleaseMask |
 			      ButtonMotionMask | EnterWindowMask |
-			      LeaveWindowMask | PointerMotionMask ),
+			      LeaveWindowMask | PointerMotionMask),
 			    GrabModeSync, GrabModeAsync, None, None,
-			    CurrentTime );
-    grabbedMouse = ( ret == GrabSuccess );
+			    CurrentTime);
+    grabbedMouse = (ret == GrabSuccess);
     return grabbedMouse;
 }
 
 void Widget::ungrabMouse()
 {
-    if ( ! grabbedMouse )
+    if (! grabbedMouse)
 	return;
 
-    XUngrabPointer( *BaseDisplay::instance(), CurrentTime );
+    XUngrabPointer(*BaseDisplay::instance(), CurrentTime);
     grabbedMouse = false;
 }
 
 bool Widget::grabKeyboard()
 {
-    int ret = XGrabKeyboard( *BaseDisplay::instance(), win, True,
-			     GrabModeSync, GrabModeAsync, CurrentTime );
-    grabbedKeyboard = ( ret == GrabSuccess );
+    int ret = XGrabKeyboard(*BaseDisplay::instance(), win, True,
+			     GrabModeSync, GrabModeAsync, CurrentTime);
+    grabbedKeyboard = (ret == GrabSuccess);
     return grabbedKeyboard;
 }
 
 void Widget::ungrabKeyboard()
 {
-    if ( ! grabbedKeyboard )
+    if (! grabbedKeyboard)
 	return;
 
-    XUngrabKeyboard( *BaseDisplay::instance(), CurrentTime );
+    XUngrabKeyboard(*BaseDisplay::instance(), CurrentTime);
     grabbedKeyboard = false;
 }
 
-void Widget::setBackgroundColor( const BColor &color )
+void Widget::setBackgroundColor(const BColor &color)
 {
-    XSetWindowBackground( *BaseDisplay::instance(), win, color.pixel() );
+    XSetWindowBackground(*BaseDisplay::instance(), win, color.pixel());
 }
 
-void Widget::buttonPressEvent( XEvent * )
-{
-}
-
-void Widget::buttonReleaseEvent( XEvent * )
+void Widget::buttonPressEvent(XEvent *)
 {
 }
 
-void Widget::pointerMotionEvent( XEvent * )
+void Widget::buttonReleaseEvent(XEvent *)
 {
 }
 
-void Widget::keyPressEvent( XEvent * )
+void Widget::pointerMotionEvent(XEvent *)
 {
 }
 
-void Widget::keyReleaseEvent( XEvent * )
+void Widget::keyPressEvent(XEvent *)
 {
 }
 
-void Widget::configureEvent( XEvent *e )
+void Widget::keyReleaseEvent(XEvent *)
 {
-    _rect.setRect( e->xconfigure.x,
+}
+
+void Widget::configureEvent(XEvent *e)
+{
+    _rect.setRect(e->xconfigure.x,
 		   e->xconfigure.y,
 		   e->xconfigure.width,
-		   e->xconfigure.height );
+		   e->xconfigure.height);
 }
 
-void Widget::mapEvent( XEvent * )
+void Widget::mapEvent(XEvent *)
 {
     visible = true;
 }
 
-void Widget::unmapEvent( XEvent * )
+void Widget::unmapEvent(XEvent *)
 {
     visible = false;
 }
 
-void Widget::focusInEvent( XEvent * )
+void Widget::focusInEvent(XEvent *)
 {
     focused = true;
 }
 
-void Widget::focusOutEvent( XEvent * )
+void Widget::focusOutEvent(XEvent *)
 {
     focused = false;
 }
 
-void Widget::exposeEvent( XEvent * )
+void Widget::exposeEvent(XEvent *)
 {
 }
 
-void Widget::enterEvent( XEvent * )
+void Widget::enterEvent(XEvent *)
 {
 }
 
-void Widget::leaveEvent( XEvent * )
+void Widget::leaveEvent(XEvent *)
 {
 }
