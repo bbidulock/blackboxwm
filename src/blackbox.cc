91d3c2e3001a46e40e6fbb2d487604c9dfbfc07f
bradleyhughes <bradleyhughes>
2002-04-19 16:13:30 +0000

    don't ignore focus events that happen during a grab, only focus changes as a result of a grab or ungrab... this keeps focus from going screwy when using bbkeys to switch workspaces

--- src/blackbox.cc
+++ src/blackbox.cc
@@ -540,7 +540,8 @@ void Blackbox::process_event(XEvent *e) {
   }
 
   case FocusIn: {
-    if (e->xfocus.mode != NotifyNormal || e->xfocus.detail == NotifyPointer)
+    if (e->xfocus.mode == NotifyGrab || e->xfocus.mode == NotifyUngrab ||
+        e->xfocus.detail == NotifyPointer)
       break;
     BlackboxWindow *win = searchWindow(e->xfocus.window);
     if (win && ! win->isFocused()) {
@@ -550,7 +551,8 @@ void Blackbox::process_event(XEvent *e) {
   }
 
   case FocusOut: {
-    if (e->xfocus.mode != NotifyNormal || e->xfocus.detail != NotifyNonlinear )
+    if (e->xfocus.mode == NotifyGrab || e->xfocus.mode == NotifyUngrab ||
+        e->xfocus.detail != NotifyNonlinear )
       break;
     BlackboxWindow *win = searchWindow(e->xfocus.window);
     if (win && win->isFocused()) {
