b9d57afd4f7f89dddc3a5e0aa8f0afe165b53f6e
bradleyhughes <bradleyhughes>
2002-05-08 13:28:48 +0000

    copyright information for bsetroot

--- util/bsetroot.cc
+++ util/bsetroot.cc
@@ -1,3 +1,26 @@
+// -*- mode: C++; indent-tabs-mode: nil; c-basic-offset: 2; -*-
+// Window.cc for Blackbox - an X11 Window manager
+// Copyright (c) 2001 - 2002 Sean 'Shaleh' Perry <shaleh at debian.org>
+// Copyright (c) 1997 - 2000, 2002 Brad Hughes <bhughes at trolltech.com>
+//
+// Permission is hereby granted, free of charge, to any person obtaining a
+// copy of this software and associated documentation files (the "Software"),
+// to deal in the Software without restriction, including without limitation
+// the rights to use, copy, modify, merge, publish, distribute, sublicense,
+// and/or sell copies of the Software, and to permit persons to whom the
+// Software is furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL
+// THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+// FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+// DEALINGS IN THE SOFTWARE.
+
 #ifdef    HAVE_CONFIG_H
 #  include "../config.h"
 #endif // HAVE_CONFIG_H
@@ -283,21 +306,23 @@ void bsetroot::gradient(void) {
 
 
 void bsetroot::usage(int exit_code) {
-  fprintf(stderr,
-          i18n(bsetrootSet, bsetrootUsage,
-	       "%s 2.0 : (c) 1997-1999 Brad Hughes\n\n"
-	       "  -display <string>        display connection\n"
-	       "  -mod <x> <y>             modula pattern\n"
-	       "  -foreground, -fg <color> modula foreground color\n"
-	       "  -background, -bg <color> modula background color\n\n"
-	       "  -gradient <texture>      gradient texture\n"
-	       "  -from <color>            gradient start color\n"
-	       "  -to <color>              gradient end color\n\n"
-	       "  -solid <color>           solid color\n\n"
-	       "  -help                    print this help text and exit\n"),
-	  getApplicationName());
+    fprintf(stderr,
+	    i18n(bsetrootSet, bsetrootUsage,
+		 "%s 2.0\n\n"
+		 "Copyright (c) 1997-2000, 2002 Bradley T Hughes\n"
+		 "Copyright (c) 2001-2002 Sean 'Shaleh' Perry\n\n"
+		 "  -display <string>        display connection\n"
+		 "  -mod <x> <y>             modula pattern\n"
+		 "  -foreground, -fg <color> modula foreground color\n"
+		 "  -background, -bg <color> modula background color\n\n"
+		 "  -gradient <texture>      gradient texture\n"
+		 "  -from <color>            gradient start color\n"
+		 "  -to <color>              gradient end color\n\n"
+		 "  -solid <color>           solid color\n\n"
+		 "  -help                    print this help text and exit\n"),
+	    getApplicationName());
 
-  exit(exit_code);
+    exit(exit_code);
 }
 
 int main(int argc, char **argv) {
