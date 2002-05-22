ff0676f82f1c7dece69808970862bee5c7f27a58
shaleh <shaleh>
2002-05-22 23:08:36 +0000

    nls Makefile updates and the addition of Ukrainian nls file

---
+++ nls/uk_UA/main.m
@@ -0,0 +1,21 @@
+$set 14 #main
+
+$ #RCRequiresArg
+# помилка: "-rc" вимага╓ аргумент\n
+$ #DISPLAYRequiresArg
+# помилка: "-display" вимага╓ аргумент\n
+$ #WarnDisplaySet
+# попередження: не вдалося встановити зм╕нну середовища "DISPLAY"\n
+$ #Usage
+# Blackbox %s: (c) 1997 - 2000 Brad Hughes\n\n\
+  -display <string>\t\tобслуговувати вказаний дисплей.\n\
+  -rc <string>\t\t\tвикористовувати альтернативний файл ресурс╕в.\n\
+  -version\t\t\tвивести номер верс╕╖ ╕ вийти.\n\
+  -help\t\t\t\tвивести цю п╕дказку ╕ вийти.\n\n
+$ #CompileOptions
+# Compile time options:\n\
+  Debugging\t\t\t%s\n\
+  Interlacing:\t\t\t%s\n\
+  Shape:\t\t\t%s\n\
+  Slit:\t\t\t\t%s\n\
+  8bpp Ordered Dithering:\t%s\n\n
