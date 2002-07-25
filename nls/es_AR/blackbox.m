20709bd46520755bd42c5c67461c313553ebd3ab
shaleh <shaleh>
2002-07-25 15:40:06 +0000

    added es_AR as it is different enough from es_ES to warrant its own translation

---
+++ nls/es_AR/blackbox.m
@@ -0,0 +1,6 @@
+$set 13 #blackbox
+
+$ #NoManagableScreens
+# Blackbox::Blackbox: no se encontraron pantallas manejables, abortando\n
+$ #MapRequest
+# Blackbox::process_event: MapRequest para 0x%lx\n
