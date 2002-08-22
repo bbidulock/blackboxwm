cc2774ce8fe113e186d61d8a384c090d817044ac
shaleh <shaleh>
2002-08-22 19:11:36 +0000

    hu_HU updates

--- nls/hu_HU/Configmenu.m
+++ nls/hu_HU/Configmenu.m
@@ -16,12 +16,16 @@ $ #FocusNew
 # Új ablak kapja a fókuszt
 $ #FocusLast
 # Munkaasztal váltása után fókusz az utolsó ablakon
+$ #DisableBindings
+# Scroll Lock-nál nem figyeli a billentyûzetet
 $ #ClickToFocus
 # Kattintás fókuszhoz
 $ #SloppyFocus
 # A fókusz követi az egeret
 $ #AutoRaise
 # Autómatikus elõreugró ablakok
+$ #ClickRaise
+# Kattintással az elõtérbe
 $ #SmartRows
 # Optimális ablakrendezés (sorok)
 $ #SmartCols
--- nls/hu_HU/main.m
+++ nls/hu_HU/main.m
@@ -7,8 +7,8 @@ $ #DISPLAYRequiresArg
 $ #WarnDisplaySet
 # Figyelem: nem lehet a 'DISPLAY' környezeti változót beállítani\n
 $ #Usage
-# Blackbox %s: (c) 2001 - 2002 Sean 'Shaleh' Perry\n\
-  \t\t\t 1997 - 2000 Brad Hughes\n\n\
+# Blackbox %s : (c) 2001 - 2002 Sean 'Shaleh' Perry\n\
+  \t\t\t 1997 - 2000, 2002 Brad Hughes\n\n\
   -display <string>\t\thasználandó kijelzõ\n\
   -rc <string>\t\t\tválasztható konfigurációs file.\n\
   -version\t\t\tverziószám kijelzése és kilépés.\n\
