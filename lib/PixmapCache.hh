37baa2a49e91a11524a7ed9fd0cec5dd0546d120
bradleyhughes <bradleyhughes>
2003-01-08 09:25:11 +0000

    document bt::PixmapCache

--- lib/PixmapCache.hh
+++ lib/PixmapCache.hh
@@ -31,21 +31,53 @@ namespace bt {
 
   class PixmapCache {
   public:
-    // maximum amount of memory to store in server side pixmaps (in kb)
-    // defaults to 2mb
+    /*
+      Returns the cache limit in kilobytes.  The default is one
+      megabyte (1024 kilobytes).
+
+      When this limit is reached, the cache will automatically free
+      unused pixmaps to reduce the amount of server side memory used.
+
+      NOTE: This limit is not a hard limit.  The cache will never free
+      a pixmap that is in use.  This means it is possible that the
+      PixmapCache memory usage can exceed this limit.
+    */
     static unsigned long cacheLimit(void);
-    // modify the above
+
+    /*
+      Set the cache limit to the specified value (in kilobytes).
+    */
     static void setCacheLimit(unsigned long limit);
 
-    // current amount of memory used to store server side pixmaps (in kb)
+    /*
+      Returns the current amount of memory in kilobytes used by the X
+      server for the pixmaps in the cache.
+    */
     static unsigned long memoryUsage(void);
 
+    /*
+      Returns a pixmap matching the specified texture and size on the
+      specified screen.  The pixmap will be rendered if necessary.
+
+      If old_pixmap is non-zero, PixmapCache::release(old_pixmap) will
+      be called immediately after finding the requested pixmap.
+    */
     static unsigned long find(unsigned int screen,
                               const Texture &texture,
                               unsigned int width, unsigned int height,
                               unsigned long old_pixmap = 0ul);
+
+    /*
+      Indicates that the specified pixmap is no longer needed.
+      Pixmaps contained in the cache are reference counted, so you
+      should call this function once for each time you call
+      PixmapCache::find().
+    */
     static void release(unsigned long pixmap);
 
+    /*
+      Free all unused pixmaps in the cache.
+    */
     static void clearCache(void);
 
   private:
