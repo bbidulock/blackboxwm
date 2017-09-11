648e88f98b1bc5431efcafbdc8bb2da16cdd1b62
Brian Bidulock <bidulock@openss7.org>
2017-09-10 19:11:13 -0600

    add branch to substitution

--- autogen.sh
+++ autogen.sh
@@ -9,11 +9,20 @@ GTVERSION=$(gettext --version|head -1|awk '{print$NF}'|sed -r 's,(^[^\.]*\.[^\.]
 if [ -x "`which git 2>/dev/null`" -a -d .git ]; then
 	VERSION=$(git describe --tags|sed 's,[-_],.,g;s,\.g.*$,,')
 	DATE=$(git show -s --format=%ci HEAD^{commit}|awk '{print$1}')
+	BRANCH=$(git tag --sort=-creatordate|head -1)
+	if [ "$VERSION" != "$BRANCH" ]; then
+		BRANCH="master"
+	fi
 	sed -i.bak configure.ac -r \
 		-e "s:AC_INIT\([[]$PACKAGE[]],[[][^]]*[]]:AC_INIT([$PACKAGE],[$VERSION]:
 		    s:AC_REVISION\([[][^]]*[]]\):AC_REVISION([$VERSION]):
 		    s:^DATE=.*$:DATE='$DATE':
+		    s:^BRANCH=.*$:BRANCH='$BRANCH':
 		    s:^AM_GNU_GETTEXT_VERSION.*:AM_GNU_GETTEXT_VERSION([$GTVERSION]):"
+	subst="s:%%PACKAGE%%:$PACKAGE:g
+	       s:%%VERSION%%:$VERSION:g
+	       s:%%DATE%%:$DATE:g
+	       s:%%BRANCH%%:$BRANCH:g"
 else
 	sed -i.bak configure.ac -r \
 		-e "s:^AM_GNU_GETTEXT_VERSION.*:AM_GNU_GETTEXT_VERSION([$GTVERSION]):"
