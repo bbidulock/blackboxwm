f79298b2b4d1c54b159cd11e84d22054ea14039b
Brian Bidulock <bidulock@openss7.org>
2019-01-18 09:17:34 -0700

    generate NEWS better

--- gennews.sh
+++ gennews.sh
@@ -28,8 +28,23 @@ for o in $(git tag --sort=-creatordate) ""; do
 	date=$(git show -s --format=%ci "$t^{commit}"|awk '{print$1}')
 	title="Release ${PACKAGE}${PACKAGE:+-}$version released $date"
 	under=$(echo "$title"|sed 's,.,-,g')
+	/usr/bin/echo -e "\n$title\n$under\n"
+	cmd="git notes --ref=release show $t"
+	notes=$(eval $cmd 2>/dev/null|sed 's,^,    ,')
+	lines=$(echo "$notes"|wc -l)
+	if [ $lines -gt 1 ] ; then
+		/usr/bin/echo -e "$cmd\n\n$notes\n"
+	fi
+	if [ :$t != :HEAD ] ; then
+		cmd="git tag -n100 -l ${t}"
+		notes=$(eval $cmd 2>/dev/null)
+		lines=$(echo "$notes"|wc -l)
+		if [ $lines -gt 1 ] ; then
+			/usr/bin/echo -e "$cmd\n\n$notes\n"
+		fi
+	fi
 	cmd="git shortlog -e -n -w80,6,8 ${o}${o:+...}${t}"
-	/usr/bin/echo -e "\n$title\n$under\n\n$cmd\n\n$(eval $cmd)\n"
+	/usr/bin/echo -e "$cmd\n\n$(eval $cmd)\n"
 	t="$o"
 done|sed -r 's,[[:space:]][[:space:]]*$,,'
 
