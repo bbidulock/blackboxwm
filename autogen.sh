a26d44b73b1bac39c98a965c342b01a487af23e3
Brian Bidulock <bidulock@openss7.org>
2019-06-17 23:11:12 -0600

    update for gettext 0.20.1 to fix issue #23

--- autogen.sh
+++ autogen.sh
@@ -4,11 +4,12 @@ cd "$(dirname "$0")"
 
 PACKAGE=$(grep AC_INIT configure.ac|head -1|sed -r 's,AC_INIT[(][[],,;s,[]].*,,')
 
-GTVERSION=$(gettext --version|head -1|awk '{print$NF}'|sed -r 's,(^[^\.]*\.[^\.]*\.[^\.]*)\..*$,\1,')
+GTVERSION=$(gettext --version|head -1|awk '{print$NF}'|sed -r 's,(^[^\.]*\.[^\.]*)\.[^\.]*$,\1,;s,(^[^\.]*\.[^\.]*\.[^\.]*)\.[^\.]*$,\1,')
 
 if [ -x "`which git 2>/dev/null`" -a -d .git ]; then
 	VERSION=$(git describe --tags|sed 's,[-_],.,g;s,\.g.*$,,')
 	DATE=$(git show -s --format=%ci HEAD^{commit}|awk '{print$1}')
+	MDOCDATE=$(perl -MDate::Format -MDate::Parse -E 'print time2str("%B %e, %Y", str2time("'"$DATE"'"))'|sed 's,  , ,')
 	BRANCH=$(git tag --sort=-creatordate|head -1)
 	GNITS="gnits "
 	if [ "$VERSION" != "$BRANCH" ]; then
@@ -19,12 +20,14 @@ if [ -x "`which git 2>/dev/null`" -a -d .git ]; then
 		-e "s:AC_INIT\([[]$PACKAGE[]],[[][^]]*[]]:AC_INIT([$PACKAGE],[$VERSION]:
 		    s:AC_REVISION\([[][^]]*[]]\):AC_REVISION([$VERSION]):
 		    s:^DATE=.*$:DATE='$DATE':
+		    s:^MDOCDATE=.*$:MDOCDATE='$MDOCDATE':
 		    s:^BRANCH=.*$:BRANCH='$BRANCH':
 		    s:^AM_GNU_GETTEXT_VERSION.*:AM_GNU_GETTEXT_VERSION([$GTVERSION]):
 		    s:^AM_INIT_AUTOMAKE\([[](gnits )?:AM_INIT_AUTOMAKE([$GNITS:"
 	subst="s:%%PACKAGE%%:$PACKAGE:g
 	       s:%%VERSION%%:$VERSION:g
 	       s:%%DATE%%:$DATE:g
+	       s:%%MDOCDATE%%:$MDOCDATE:g
 	       s:%%BRANCH%%:$BRANCH:g"
 else
 	sed -i.bak configure.ac -r \
